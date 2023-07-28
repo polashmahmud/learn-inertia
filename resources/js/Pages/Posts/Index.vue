c4c7bda24b588a5ac6dd7515b70b9b3cb21aea9a
Polash Mahmud <polashmahmud@gmail.com>
2023-07-28 19:29:32 +0600

    class 07

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,9 +1,13 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import {Head} from '@inertiajs/vue3';
+import {Head, useForm} from '@inertiajs/vue3';
 
 defineProps({
     posts: Array
+})
+
+const form = useForm({
+    body: '',
 })
 </script>
 
@@ -17,6 +21,13 @@ defineProps({
 
         <div class="py-12">
             <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-3">
+                <form class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                    {{ form }}
+                    <label for="body" class="sr-only">Body</label>
+                    <textarea v-model="form.body" name="body" id="body" cols="30" rows="5" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"></textarea>
+                    <button type="submit" class="mt-2 bg-gray-700 px-4 py-2 rounded-md font-medium text-white">Post</button>
+                </form>
+
                 <div v-for="post in posts" :key="post.id">
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 text-gray-900">
